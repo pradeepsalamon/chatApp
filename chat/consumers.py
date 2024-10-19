d5707556c373731f1fab60445cfa50f7503b9547
pradeepsalamon <pradeepsalamon14@gmail.com>
2024-10-20 00:49:50 +0530

    dapphne test

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,5 @@
 import json
-from .models import Message, Room
+# from .models import Message, Room
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 
@@ -25,15 +25,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'type': 'send_message',
             'data' : data
         }
-        await self.create_message(data = data)
+        # await self.create_message(data = data)
         await self.channel_layer.group_send(self.room_name, event)
         
     async def send_message(self, event):
         data = event['data']
         await self.send(text_data=json.dumps({'data':data}))
         
-    @database_sync_to_async
-    def create_message(self, data):
-        get_room = Room.objects.get(room_name = data['roomname'])
-        new_message = Message(room = get_room, sender = data['sender'], message = data['message'])
-        new_message.save()
+    # @database_sync_to_async
+    # def create_message(self, data):
+    #     get_room = Room.objects.get(room_name = data['roomname'])
+    #     new_message = Message(room = get_room, sender = data['sender'], message = data['message'])
+    #     new_message.save()
