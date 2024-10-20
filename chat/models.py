875ba802e5502bf407adc591de3f92818d311147
pradeepsalamon <pradeepsalamon14@gmail.com>
2024-10-20 13:52:01 +0530

    test with models

--- chat/models.py
+++ chat/models.py
@@ -13,3 +13,13 @@ class Message(models.Model):
     
     def __str__(self) -> str:
         return str(self.room)
+    
+from django.db import models
+
+class ChatMessage(models.Model):
+    user = models.CharField(max_length=100)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}: {self.message[:50]} ({self.timestamp})"
